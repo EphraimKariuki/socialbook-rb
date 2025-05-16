ba38ebbe16fdc3dd103f43400668cc6fe8fe7894
ephraimkariuki <ephraimkabuikariuki@gmail.com>
2025-05-16 18:07:13 +0300

    User's profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+    @likes = @user.likes
+    @followers = @user.followed_follows
+    @followings = @user.follower_follows
+  end
 end
